ac35fe487b977773f3b244c79252c9fb14fa59a4
alief abdillah <aliefmabdillah09@gmail.com>
2025-05-02 21:18:37 +0700

    feat: last update time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import TopCard from "@/components/Card/TopCard";
 export default function Home() {
   return (
     <BaseLayout searchBar={false}>
+      <p className="px-6 mt-4 font-montserrat font-bold">Last Update : 5 Minute Ago</p>
       <div className="grid grid-cols-4 px-6 my-6 gap-4">
         <TopCard />
       </div>
