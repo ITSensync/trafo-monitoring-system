f6ccb8dc8f9ebca0cdcdaefdc2b8c1e4fa12243f
alief abdillah <aliefmabdillah09@gmail.com>
2025-05-03 22:57:33 +0700

    chore: style component button sidebar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,25 +20,31 @@ export default function Navbar({ searchBar = true }: { searchBar?: boolean }) {
               </p>
             </div>
             <div className="h-0.5 w-full border bg-amber-500 mb-4"></div>
-            <li className="flex flex-row items-center rounded-lg hover:bg-amber-50 font-poppins mb-2  cursor-pointer">
+            <Link
+              href={"/"}
+              className="flex flex-row items-center rounded-lg hover:bg-amber-50 font-poppins mb-2 cursor-pointer gap-5 px-3 py-2"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="orange"
-                className="size-13"
+                className="size-8"
               >
                 <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
                 <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
               </svg>
 
-              <Link href="/" className="text-lg font-medium">Home</Link>
-            </li>
-            <li className="flex flex-row items-center rounded-lg hover:bg-amber-50 font-poppins cursor-pointer">
+              <p className="text-lg font-medium">Home</p>
+            </Link>
+            <Link
+              href={"/login"}
+              className="flex flex-row items-center rounded-lg hover:bg-amber-50 font-poppins cursor-pointer gap-5 px-3 py-2"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="orange"
-                className="size-13"
+                className="size-8"
               >
                 <path
                   fillRule="evenodd"
@@ -47,8 +53,8 @@ export default function Navbar({ searchBar = true }: { searchBar?: boolean }) {
                 />
               </svg>
 
-              <Link href="/login" className="text-lg font-medium">Sign Out</Link>
-            </li>
+              <p className="text-lg font-medium">Sign Out</p>
+            </Link>
           </ul>
         </div>
       </div>
