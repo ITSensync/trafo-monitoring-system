ee1fa875b7e3488fc4ca202deaf5349bb5854a41
Alief Muhammad Abdillah <aliefmabdillah09@gmail.com>
2025-07-07 15:53:54 +0700

    chore: cleaning code

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -7,7 +7,6 @@ import { dateToYYYYMMDD } from "@/lib/DateFormatter";
 
 export default function Graph() {
   const [date, setDate] = useState<Date | undefined>();
-  console.log(dateToYYYYMMDD(date ? date : new Date()));
   return (
     <div className="bg-white/95 my-5 mx-5 rounded-xl px-6 pt-5 pb-4 h-fit">
       <div className="flex flex-row items-center justify-between w-full px-5 mb-4">
